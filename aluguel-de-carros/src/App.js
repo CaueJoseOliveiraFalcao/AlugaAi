5c8f633bf323f6b7f699b6356665d2e9aec78975
Cauê Josê Oliveira Falcão <cauejose007@gmail.com>
2023-10-30 15:32:28 -0400

    ds

--- aluguel-de-carros/src/App.js
+++ aluguel-de-carros/src/App.js
@@ -1,17 +1,16 @@
 import React from "react";
-import { Redirect, Switch, Route, Router , Outlet } from "react-router-dom";
+import { Redirect, Switch, Route, Routes , Outlet , BrowserRouter } from "react-router-dom";
 import Login from './components/login/login';
 import Register from './components/register/register';
 import Dashboard from './components/dashpage/dashboard'; // Use 'Dashboard', com D maiúsculo
 import { useState } from 'react';
 import { history } from "./ult/history";
-import RouteGuard from "./components/RouteGuard";
 
 function App() { 
   return (
       <div className="App">
         <BrowserRouter>
-           <Routes history={history}>
+           <Routes>
               <Route path="/" element={<Login/>} />
               <Route path="/register" element={<Register />} />
               <Route
